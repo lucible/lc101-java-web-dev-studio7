43a07f0d62ac7e6caf305be06f883351009ace45
lucible <gobbldygook@letterboxes.org>
2020-01-29 20:33:46 -0600

    working on class 7 studio

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,10 +1,46 @@
 package org.launchcode.studio7;
 
-public class CD {
+public class CD extends BaseDisc implements OpticalDisc {
 
-    // TODO: Implement your custom interface.
+    private static final String spinRate = "200 - 500";
 
-    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-    //  need to be declared separately.
+    private String name;
+    private String contents;
 
+    // Basic constructor
+    public CD(boolean aIsBlank, boolean aIsRewritable, int aCapacity, String aName, String aContents) {
+        super(aIsBlank, aIsRewritable, aCapacity);
+        name = aName;
+        contents = aContents;
+    }
+
+    // Music CD constructor
+    public CD(String aName, String aContents) {
+        super();
+        name = aName;
+        contents = aContents;
+    }
+
+    @Override
+    public void spinDisc() {
+        System.out.println("A CD spins at a rate of " + spinRate + " rpm.");
+    }
+
+    @Override
+    public void getDiscInfo() {
+        if (this.isBlank && this.isRewritable) {
+            System.out.println("This is a blank, rewritable CD.");
+            this.printCapacity();
+        } else if (this.isBlank && !this.isRewritable) {
+            System.out.println("This is a blank, non-rewritable CD.");
+            this.printCapacity();
+        } else if (!this.isBlank && this.isRewritable) {
+            System.out.println(this.name + " is a rewritable CD.");
+            this.printCapacity();
+            System.out.println(this.name + " contains: " + this.contents);
+        } else if (!this.isBlank && !this.isRewritable) {
+            System.out.println("This is a CD named " + this.name + ".");
+            System.out.println("It contains: " + this.contents);
+        }
+    }
 }
--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -1,6 +1,30 @@
 package org.launchcode.studio7;
 
-public class DVD {
+public class DVD extends BaseDisc implements OpticalDisc {
+
+    private static final String spinRate = "570 - 1600";
+
+    private String name;
+    private String contents;
+
+    // Basic constructor
+    public DVD(boolean aIsBlank, boolean aRewritable, int aCapacity, String aName, String aContents) {
+        super(aIsBlank, aRewritable, aCapacity);
+        name = aName;
+        contents = aContents;
+    }
+
+    // Movie DVD constructor
+    public DVD(String aName, String aContents) {
+        super();
+        name = aName;
+        contents = aContents;
+    }
+
+    @Override
+    public void spinDisc() {
+        System.out.println("A DVD spins at a rate of " + spinRate + " rpm.");
+    }
 
     // TODO: Implement your custom interface.
 
